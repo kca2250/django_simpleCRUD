57a7331cf5ee8a1642d21cb246d0ca9d8f1e9538
ka2250 <2250katok@gmail.com>
2021-08-11 22:32:11 +0900

    BlogAdminSiteの編集を実施

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin.forms import AuthenticationForm
+from django.contrib.admin import AdminSite
 from . import models
 
 
@@ -16,3 +18,20 @@ class CategoryAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     pass
 
+
+class BlogAdminSite(AdminSite):
+    site_header = "マイページ"
+    site_title = "マイページ"
+    index_title = "ホーム"
+    site_url = None
+    login_form = AuthenticationForm
+
+    def has_permission(self, request):
+        return request.user.is_active
+
+
+myPage_site = BlogAdminSite(name="myPage")
+
+myPage_site.register(models.Post)
+myPage_site.register(models.Tag)
+myPage_site.register(models.Category)
